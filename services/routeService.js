6c5576f53038028a59b5e47cfc342f585fdc5b2a
ElUriela <uriel.ao@live.com>
2025-06-16 03:01:47 -0600

    fix updated sql query fields for function addroutes

--- services/routeService.js
+++ services/routeService.js
@@ -10,15 +10,28 @@ function getRoutes() {
   });
 }
 
-function addRoute({ origin, destination, schedule }) {
+function addRoute({ inicio, fin, distancia, tiempo }) {
   const id_ruta = uuidv4();
 
   return new Promise((resolve, reject) => {
-    const query = "INSERT INTO ruta (id_ruta, origen, destino, horario) VALUES (?, ?, ?, ?)";
-    db.query(query, [id_ruta, origin, destination, schedule], (err) => {
-      if (err) return reject(err);
-      resolve(id_ruta);
-    });
+    const query = `
+      INSERT INTO ruta (id_ruta, coordenadas_inicio, coordenadas_fin, distancia, tiempo_estimado)
+      VALUES (?, POINT(?, ?), POINT(?, ?), ?, ?)
+    `;
+    db.query(
+      query,
+      [
+        id_ruta,
+        inicio.lat, inicio.lng,
+        fin.lat, fin.lng,
+        distancia,
+        tiempo // formato: '00:45:00'
+      ],
+      (err) => {
+        if (err) return reject(err);
+        resolve(id_ruta);
+      }
+    );
   });
 }
 
