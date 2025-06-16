94c54387ae0c9b76ea3895b3b92bb847d48fb694
diego zamora <al261855@edu.uaa.mx>
2025-06-16 03:11:51 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,18 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  
 });
 
-module.exports = db;
+connection.connect(err => {
+  if (err) console.error("❌ Error conectando a la BD:", err);
+  else console.log("✅ Conectado a la base de datos MySQL");
+});
+
+module.exports = connection;
